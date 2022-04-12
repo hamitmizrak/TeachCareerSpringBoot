be40e46e7481d4643d6cb60859212c3252ab5029
hamitmizrak <hamitmizrak@gmail.com>
2022-04-12 23:31:32 +0300

    css,js

--- src/main/java/com/hamitmizrak/mvc/ControllerTutorials.java
+++ src/main/java/com/hamitmizrak/mvc/ControllerTutorials.java
@@ -17,17 +17,30 @@ public class ControllerTutorials {
     }
 
 
+    //th:text
     // http://localhost:8080/templates/thymeleaf1
     @GetMapping("templates/thymeleaf1")
     public String getThymleaf1() {
-        return "_01_thymeleaf";
+        return "_01_th_text";
     }
 
+    //Model
     // http://localhost:8080/templates/thymeleaf2
     @GetMapping("templates/thymeleaf2")
     public String getThymleaf2(Model model) {
         model.addAttribute("key","Model Merhabalar Nasılsınız");
-        return "_02_thymeleaf";
+        return "_02_model";
     }
+
+    //messages.properties
+    //css js
+    // http://localhost:8080/templates/thymeleaf3
+    @GetMapping("templates/thymeleaf3")
+    public String getThymleaf3(Model model) {
+        model.addAttribute("key","Model Merhabalar Nasılsınız");
+        return "_03_css_js";
+    }
+
+
 
 }

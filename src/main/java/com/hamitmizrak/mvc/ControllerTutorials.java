d9dfca9da5bb51c92c814f6fc93f8cef9d9004c1
hamitmizrak <hamitmizrak@gmail.com>
2022-04-12 23:05:03 +0300

    @Controller

--- src/main/java/com/hamitmizrak/mvc/ControllerTutorials.java
+++ src/main/java/com/hamitmizrak/mvc/ControllerTutorials.java
@@ -1,6 +1,7 @@
 package com.hamitmizrak.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,4 +15,19 @@ public class ControllerTutorials {
     public String getRoot(){
         return "Root Bileşeni";
     }
+
+
+    // http://localhost:8080/templates/thymeleaf1
+    @GetMapping("templates/thymeleaf1")
+    public String getThymleaf1() {
+        return "_01_thymeleaf";
+    }
+
+    // http://localhost:8080/templates/thymeleaf2
+    @GetMapping("templates/thymeleaf2")
+    public String getThymleaf2(Model model) {
+        model.addAttribute("key","Model Merhabalar Nasılsınız");
+        return "_02_thymeleaf";
+    }
+
 }

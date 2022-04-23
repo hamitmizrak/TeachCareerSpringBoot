4a2f440a6d0b4864213aeee05e0d8e8876190e0f
hamitmizrak <hamitmizrak@gmail.com>
2022-04-23 22:30:16 +0300

    ResponseEntity

--- src/main/java/com/hamitmizrak/rest/ComputerClient.java
+++ src/main/java/com/hamitmizrak/rest/ComputerClient.java
@@ -3,6 +3,9 @@ package com.hamitmizrak.rest;
 import com.hamitmizrak.dto.ComputerDto;
 import com.hamitmizrak.dto.ProductDto;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,6 +39,20 @@ public class ComputerClient {
         RestTemplate restTemplate=new RestTemplate();
         ComputerDto computerDto=restTemplate.getForObject(URL,ComputerDto.class);
         return computerDto;
+    }
+
+
+
+    //ResponseEntity
+    //http://localhost:8080/client3
+    @GetMapping("client3")
+    @ResponseBody
+    public ComputerDto client3() {
+        String URL="http://localhost:8080/rest2";
+        RestTemplate restTemplate=new RestTemplate();
+        ResponseEntity<ComputerDto> responseEntity=restTemplate.exchange(URL, HttpMethod.GET, HttpEntity.EMPTY,ComputerDto.class);
+        ComputerDto computerBody=responseEntity.getBody();
+        return computerBody;
     }
 
 

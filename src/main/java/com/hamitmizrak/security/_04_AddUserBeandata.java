d9c69eb3f15bf458e6bb5d4f79fffea99d6c1e91
hamitmizrak <hamitmizrak@gmail.com>
2022-04-28 22:19:31 +0300

    Authentication

--- src/main/java/com/hamitmizrak/security/_04_AddUserBeandata.java
+++ src/main/java/com/hamitmizrak/security/_04_AddUserBeandata.java
@@ -1,28 +1,26 @@
 package com.hamitmizrak.security;
 
+import com.hamitmizrak.beans.PasswordBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
-public class _03_AddUserEncryped{
+public class _04_AddUserBeandata{
 
 //@EnableWebSecurity
-//public class _03_AddUserEncryped extends WebSecurityConfigurerAdapter {
+//public class _04_AddUserBeandata extends WebSecurityConfigurerAdapter {
+//
+//   @Autowired
+//   PasswordBean passwordBean;
 //
 //   @Autowired
 //   public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
-//
-//      //Encoder Decoder
-//      PasswordEncoder passwordEncoder= PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//
 //      authenticationManagerBuilder
 //              .inMemoryAuthentication()
 //              .withUser("hamitmizrak")
-//              .password( passwordEncoder.encode("root"))
+//              .password(passwordBean.passwordEncoder().encode("root"))
 //              .roles("ADMIN");
 //
 //   }
---
+++ src/main/java/com/hamitmizrak/security/_05_Authentication.java
@@ -0,0 +1,37 @@
+package com.hamitmizrak.security;
+
+import com.hamitmizrak.beans.PasswordBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class _05_Authentication extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                //dikkkattt: antMatchers ==> @GetMappingten gelen url yazılır.
+                .antMatchers("/", "/index", "/security/public", "/security/success")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    PasswordBean passwordBean;
+
+    @Autowired
+    public void configureGlobalAddUser(AuthenticationManagerBuilder managerBuilder) throws Exception {
+        managerBuilder
+                .inMemoryAuthentication()
+                .withUser("hamitmizrak")
+                .password(passwordBean.passwordEncoder().encode("root"))
+                .roles("USER");
+    }
+}

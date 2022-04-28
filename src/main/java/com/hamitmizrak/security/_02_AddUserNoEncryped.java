77b73362839425ac443ef4930b83672b0ec88f11
hamitmizrak <hamitmizrak@gmail.com>
2022-04-28 21:23:50 +0300

    AddUser,Encryped , roles

---
+++ src/main/java/com/hamitmizrak/security/_02_AddUserNoEncryped.java
@@ -0,0 +1,22 @@
+package com.hamitmizrak.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+public class _02_AddUserNoEncryped{
+//@EnableWebSecurity
+//public class _02_AddUserNoEncryped extends WebSecurityConfigurerAdapter {
+//
+//   @Autowired
+//   public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+//      authenticationManagerBuilder
+//              .inMemoryAuthentication()
+//              .withUser("hamitmizrak")
+//              .password("{noop}root")
+//              .roles("ADMIN");
+//
+//   }
+}
--- src/main/java/com/hamitmizrak/security/_03_AddUserEncryped.java
+++ src/main/java/com/hamitmizrak/security/_03_AddUserEncryped.java
@@ -1,21 +1,25 @@
 package com.hamitmizrak.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class _02_AddUser extends WebSecurityConfigurerAdapter {
+public class _03_AddUserEncryped extends WebSecurityConfigurerAdapter {
 
    @Autowired
    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+
+      //Encoder Decoder
+      PasswordEncoder passwordEncoder= PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
       authenticationManagerBuilder
               .inMemoryAuthentication()
               .withUser("hamitmizrak")
-              .password("{noop}root")
+              .password( passwordEncoder.encode("root"))
               .roles("ADMIN");
 
    }
